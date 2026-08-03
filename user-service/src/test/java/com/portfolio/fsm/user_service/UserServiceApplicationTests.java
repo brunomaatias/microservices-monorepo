7b57fde254a82da9ac3c4579799638b5dd9f633e
brunomaatias <bruno.gabriel0589@gmail.com>
2026-08-03 20:12:03 -0300

    creationa and configuration of the microservice 'user-service'.

--- user-service/src/test/java/com/portfolio/fsm/user_service/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/portfolio/fsm/user_service/UserServiceApplicationTests.java
@@ -1,9 +1,11 @@
 package com.portfolio.fsm.user_service;
+import org.springframework.test.context.ActiveProfiles;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UserServiceApplicationTests {
 
 	@Test
